4e728e485261de773e0959c8a41c5819b2c558ea
artihepat7 <hepatarti@gmail.com>
2020-08-07 10:17:54 +0530

    Hangman Game

---
+++ app.js
@@ -0,0 +1,7 @@
+const wrong_letters = document.getElementById("wrong-letters");
+const word = document.getElementById("word");
+const popup_container = document.getElementById("popup-container");
+const notification_container = document.getElementById(
+  "notification-container"
+);
+const wrong_letters = document.getElementById("wrong-letters");
